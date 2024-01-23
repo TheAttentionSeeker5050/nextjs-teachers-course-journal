5be18af08feb7c9aaa292e3af1e16cd80ffbd08a
Oladoyin Fagbohun <doyinma@gmail.com>
2024-01-23 13:59:38 -0400

    finished initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+import prisma from "@/data/prisma";
+
+export const getServerSideProps = async () => {
+  const feed = await prisma.post.findMany({
+    
+  })
+  return { props: { feed } }
+}
+
+export default function Home(props) {
+
+  console.log(props.feed)
+
+  return (
+    <main
+      className={`${inter.className}`}
+    >
+      <h1 className="">
+        Title
+      </h1>
+
+      <div className="flex flex-col">
+        {props.feed.map((post) => (
+          
+          <div key={post.id} className="flex flex-col">
+            <h2 className="text-2xl font-bold">
+              {post.title}
+            </h2>
+            <p className="text-xl">
+              {post.content}
+            </p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
