70a847c8373abbe6ef01480e9903c3d66c7c9e3c
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-01-26 15:35:09 -0400

    added the unit model and db transaction

---
+++ __tests__/unitsCrudTransactions.test.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+
+
+// create a test that will test if true is equal to true
+describe("Unit CRUD Transactions", () => {
+    it('true is equal to true', () => {
+    expect(true).toBe(true)
+    })
+})
