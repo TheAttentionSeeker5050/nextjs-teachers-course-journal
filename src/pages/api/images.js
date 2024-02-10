f09b9849caaec24ffec2937942faf4f4f20376a0
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-10 15:03:26 -0400

    before refining the layout

--- src/pages/api/images.js
+++ src/pages/api/images.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 
 // this will feed the image to the Image components on the page
+// later on this project, we will keep track of the images and their owners
+// in the database, and verify if the user has access to the image
 export default function handler(req, res) {
     // get the image name url parameter
     const { imageName } = req.query;
@@ -10,8 +12,10 @@ export default function handler(req, res) {
 
     // get the image from the folder cwd/src/media/images
     const img = fs.readFileSync(`${cwd}/src/media/images/${imageName}`);
-    // send the image to the client
-
+    
+    // add the content type header so it forces the response to be an image
     res.setHeader('Content-Type', 'image/png');
+    
+    // send the image to the client
     res.status(200).send(img);
 }
