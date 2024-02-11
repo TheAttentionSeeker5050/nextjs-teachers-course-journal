5427d4194bfd07778dc754c11d9109c3f5389355
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-11 17:52:29 -0400

    Added custom http error pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import Navbar from '@/components/Navbar';
 import Link from 'next/link';
 
 
-export default function Unauthorized() {
+export default function Custom404() {
 
 
     return (
@@ -17,13 +17,13 @@ export default function Unauthorized() {
         <Navbar isLoggedIn={false} />
 
         <h1 className="text-main-title-size font-semibold text-primary-600 text-center my-3">
-          User is not authorized 
+          Page was not found
         </h1>
 
-        <p className="text-tertiary-title-size">
+        <p className="text-tertiary-title-size ">
           Please 
-          <Link href="/login" className='hover:text-primary-300 text-primary-500'> login </Link>
-           to access this page
+          <Link href="/" className='hover:text-primary-300 text-primary-500'> click here </Link>
+           to go back to the main page
         </p>
   
         
