48328b890facf1f096b118edd6f7d992048a6f90
Oladoyin <doyinma@gmail.com>
2024-02-12 05:51:21 -0400

    changed navbar link to signup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,21 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Navbar = ({isLoggedIn}) => {
+const Navbar = ({ isLoggedIn }) => {
     return (
         <nav className="navbar flex flex-row gap-4 justify-center text-tertiary-title-size bg-primary-600 w-screen py-4 text-white">
-            
+
             <Link href="/" className='hover:text-slate-300'>Home</Link>
-            
-            {isLoggedIn ? 
+
+            {isLoggedIn ?
                 <Link href="/logout" className='hover:text-slate-300'>Logout</Link>
-            :
+                :
                 <>
                     <Link href="/login" className='hover:text-slate-300'>Login</Link>
-                    <Link href="/register" className='hover:text-slate-300'>Register</Link>
+                    <Link href="/signup" className='hover:text-slate-300'>Sign Up</Link>
                 </>
             }
-            
+
         </nav>
     );
 }
