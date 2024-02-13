35fde04d0b3cf26d5d644f50350beb1a5dd49095
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-13 13:12:32 -0400

    fixed the database 1

--- postgres-data/db-backup.sql
+++ postgres-data/db-backup.sql
@@ -1,9 +1,4 @@
--- Adminer 4.8.1 PostgreSQL 16.1 dump
 
--- \connect "courseGridDB";
-
-DROP TABLE IF EXISTS "courses";
-DROP SEQUENCE IF EXISTS courses_id_seq;
 CREATE SEQUENCE courses_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 
 CREATE TABLE "public"."courses" (
@@ -16,6 +11,73 @@ CREATE TABLE "public"."courses" (
     "thumbnail" character varying(250),
     CONSTRAINT "courses_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
+
+
+CREATE SEQUENCE lessons_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."lessons" (
+    "id" integer DEFAULT nextval('lessons_id_seq') NOT NULL,
+    "lessonName" text NOT NULL,
+    "lessonNumber" integer NOT NULL,
+    "completionStatus" text DEFAULT 'not prepped' NOT NULL,
+    "epectedOutcomes" text,
+    "assessment" text,
+    "dateCreated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
+    "dateUpdated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
+    "unitId" integer NOT NULL,
+    CONSTRAINT "lessons_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+CREATE SEQUENCE thumbnails_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."thumbnails" (
+    "id" integer DEFAULT nextval('thumbnails_id_seq') NOT NULL,
+    "thumbnail" character varying(250) NOT NULL,
+    "userId" integer NOT NULL,
+    CONSTRAINT "thumbnails_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+CREATE SEQUENCE units_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."units" (
+    "id" integer DEFAULT nextval('units_id_seq') NOT NULL,
+    "unitNumber" integer NOT NULL,
+    "unitName" text NOT NULL,
+    "courseId" integer NOT NULL,
+    "dateCreated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
+    "dateUpdated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "units_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."users" (
+    "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
+    "email" text NOT NULL,
+    "password" text NOT NULL,
+    "firstName" text,
+    "lastName" text,
+    "title" text,
+    "organization" text,
+    "dateCreated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
+    "dateUpdated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "users_email_key" UNIQUE ("email"),
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+ALTER TABLE ONLY "public"."courses" ADD CONSTRAINT "courses_userId_fkey" FOREIGN KEY ("userId") REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+
+ALTER TABLE ONLY "public"."lessons" ADD CONSTRAINT "lessons_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES units(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+
+ALTER TABLE ONLY "public"."thumbnails" ADD CONSTRAINT "thumbnails_userId_fkey" FOREIGN KEY ("userId") REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+
+ALTER TABLE ONLY "public"."units" ADD CONSTRAINT "units_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES courses(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+
+INSERT INTO "users" ("id", "email", "password", "firstName", "lastName", "title", "organization", "dateCreated", "dateUpdated") VALUES
+(1,	'iamseverous@hogwardsschoolofmagic.com',	'$2b$10$1LqxuTPcdW5AMr8V7BsZ6OnPvEc1/lRcS3mdNJ05lMA9nJxIxD4kq',	'Severous',	'Snape',	'Defense Against the Dark Arts',	'Hogwards',	'2024-01-25 21:11:44.275',	'2024-01-25 21:11:44.275');
 
 INSERT INTO "courses" ("id", "courseName", "userId", "dateCreated", "dateUpdated", "isArchived", "thumbnail") VALUES
 (1,	'Defence Against the Dark Arts',	1,	'2024-02-10 17:50:28.182',	'2024-02-10 17:50:28.182',	'f',	NULL),
@@ -51,80 +113,19 @@ INSERT INTO "courses" ("id", "courseName", "userId", "dateCreated", "dateUpdated
 (31,	'Potions',	1,	'2024-02-10 17:51:44.39',	'2024-02-10 17:51:44.39',	'f',	NULL),
 (32,	'Potions',	1,	'2024-02-10 17:51:44.39',	'2024-02-10 17:51:44.39',	'f',	NULL);
 
-DROP TABLE IF EXISTS "lessons";
-DROP SEQUENCE IF EXISTS lessons_id_seq;
-CREATE SEQUENCE lessons_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."lessons" (
-    "id" integer DEFAULT nextval('lessons_id_seq') NOT NULL,
-    "lessonName" text NOT NULL,
-    "lessonNumber" integer NOT NULL,
-    "completionStatus" text DEFAULT 'not prepped' NOT NULL,
-    "epectedOutcomes" text,
-    "assessment" text,
-    "dateCreated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
-    "dateUpdated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
-    "unitId" integer NOT NULL,
-    CONSTRAINT "lessons_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
+INSERT INTO "units" ("id", "unitNumber", "unitName", "courseId", "dateCreated", "dateUpdated") VALUES
+(1,	1,	'Unit Name 1',	1,	'2024-02-13 13:56:34.076',	'2024-02-13 13:56:34.076'),
+(2,	2,	'Unit Name 2',	1,	'2024-02-13 13:56:41.59',	'2024-02-13 13:56:41.59'),
+(3,	3,	'Unit Name 3',	1,	'2024-02-13 13:56:51.283',	'2024-02-13 13:56:51.283'),
+(4,	4,	'Unit Name 4',	1,	'2024-02-13 13:57:03.042',	'2024-02-13 13:57:03.042');
 
 
-DROP TABLE IF EXISTS "thumbnails";
-DROP SEQUENCE IF EXISTS thumbnails_id_seq;
-CREATE SEQUENCE thumbnails_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."thumbnails" (
-    "id" integer DEFAULT nextval('thumbnails_id_seq') NOT NULL,
-    "thumbnail" character varying(250) NOT NULL,
-    "userId" integer NOT NULL,
-    CONSTRAINT "thumbnails_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-INSERT INTO "thumbnails" ("id", "thumbnail", "userId") VALUES
-(1,	'course-image.png',	1);
-
-DROP TABLE IF EXISTS "units";
-DROP SEQUENCE IF EXISTS units_id_seq;
-CREATE SEQUENCE units_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."units" (
-    "id" integer DEFAULT nextval('units_id_seq') NOT NULL,
-    "unitNumber" integer NOT NULL,
-    "unitName" text NOT NULL,
-    "courseId" integer NOT NULL,
-    "dateCreated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
-    "dateUpdated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "units_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "users";
-DROP SEQUENCE IF EXISTS users_id_seq;
-CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."users" (
-    "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
-    "email" text NOT NULL,
-    "password" text NOT NULL,
-    "firstName" text,
-    "lastName" text,
-    "title" text,
-    "organization" text,
-    "dateCreated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
-    "dateUpdated" timestamp(3) DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "users_email_key" UNIQUE ("email"),
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-INSERT INTO "users" ("id", "email", "password", "firstName", "lastName", "title", "organization", "dateCreated", "dateUpdated") VALUES
-(1,	'iamseverous@hogwardsschoolofmagic.com',	'$2b$10$1LqxuTPcdW5AMr8V7BsZ6OnPvEc1/lRcS3mdNJ05lMA9nJxIxD4kq',	'Severous',	'Snape',	'Defense Against the Dark Arts',	'Hogwards',	'2024-01-25 21:11:44.275',	'2024-01-25 21:11:44.275');
-
-ALTER TABLE ONLY "public"."courses" ADD CONSTRAINT "courses_userId_fkey" FOREIGN KEY ("userId") REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-
-ALTER TABLE ONLY "public"."lessons" ADD CONSTRAINT "lessons_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES units(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-
-ALTER TABLE ONLY "public"."thumbnails" ADD CONSTRAINT "thumbnails_userId_fkey" FOREIGN KEY ("userId") REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-
-ALTER TABLE ONLY "public"."units" ADD CONSTRAINT "units_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES courses(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
-
--- 2024-02-12 14:20:52.976588+00
+INSERT INTO "lessons" ("id", "lessonName", "lessonNumber", "completionStatus", "epectedOutcomes", "assessment", "dateCreated", "dateUpdated", "unitId") VALUES
+(1,	'Lesson Name 1',	1,	'not prepped',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 13:59:26.841',	'2024-02-13 13:59:26.841',	1),
+(2,	'Lesson Name 2',	2,	'not prepped',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:00:41.818',	'2024-02-13 14:00:41.818',	1),
+(3,	'Lesson Name 3',	3,	'prepped',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:01:05.337',	'2024-02-13 14:01:05.337',	2),
+(4,	'Lesson Name 4',	4,	'done',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:01:22.764',	'2024-02-13 14:01:22.764',	2),
+(5,	'Lesson Name 5',	5,	'done',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:01:47.124',	'2024-02-13 14:01:47.124',	3),
+(6,	'Lesson Name 6',	6,	'not prepped',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:02:07.912',	'2024-02-13 14:02:07.912',	3),
+(7,	'Lesson Name 7',	7,	'done',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:02:39.538',	'2024-02-13 14:02:39.538',	4),
+(8,	'Lesson Name 8',	8,	'not prepped',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veniam sint fugit, animi ipsum, natus ex quod pariatur aut, repellendus harum totam nulla nostrum commodi.',	'2024-02-13 14:02:59.13',	'2024-02-13 14:02:59.13',	4);
