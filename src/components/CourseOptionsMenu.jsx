1c157ec1da705ec1d502d4b2293cb2e05d531e8b
Oladoyin <doyinma@gmail.com>
2024-03-20 00:51:59 -0300

    fix course option menu

--- src/components/CourseOptionsMenu.jsx
+++ src/components/CourseOptionsMenu.jsx
@@ -18,7 +18,7 @@ const CourseOptionsMenu = ({ courseId }) => {
                 </svg>
             </button>
             {isOpen && (
-                <div className="absolute top-10 right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+                <div className="absolute top-10 right-3 mt-2 w-46 bg-white border border-gray-200 rounded-md shadow-lg z-10">
                     <Link href={`/course/${courseId}/edit/`} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                         Edit Course
                     </Link>
