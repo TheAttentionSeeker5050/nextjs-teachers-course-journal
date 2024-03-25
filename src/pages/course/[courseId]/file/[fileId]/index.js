1d438012e1d43f9b7dd948706e756b952ad527d7
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-03-25 20:05:15 -0300

    Add file download functionality and display file list in lesson page

--- src/pages/course/[courseId]/file/[fileId]/index.js
+++ src/pages/course/[courseId]/file/[fileId]/index.js
@@ -1,24 +1,96 @@
+import { useEffect } from 'react';
+import prisma from '@/data/prisma';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 export async function getServerSideProps(context) {
 
     // get courseId, lessonId, and fileId from the context
     const { courseId, fileId } = context.query;
 
+    // get user payload from the headers
+    const userPayloadStr = context.req.headers['x-user-payload'];
+    //parse user payload
+    const userPayload = JSON.parse(userPayloadStr);
+    try {
+    // search the file on db record
+    const fileRecord = await prisma.fileUpload.findUnique({
+        where: {
+          id: parseInt(fileId),
+        },
+    });
+
+    if (!fileRecord) {
+        return {
+            redirect: {
+                destination: '/404',
+                permanent: false
+            }
+        }
+    }
+
+    // if the user is not the owner of the course, redirect to 404
+    if (fileRecord.userId !== userPayload.userId) {
+        return {
+            redirect: {
+                destination: '/unauthorized',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
             foo: 'bar',
             courseId,
-            fileId
+            fileId,
+            fileName: fileRecord.fileDisplayName,
+        }
+    }
+    } catch (error) {
+        return {
+            redirect: {
+                destination: '/500',
+                permanent: false
+            }
         }
     }
 }
 
+
 export default function DownloadFile(props) {
+    const { courseId, fileId, fileName } = props;
+    const router = useRouter();
+    const [downloaded, setDownloaded] = useState(false);
+  
+    useEffect(() => {
+        if (downloaded) return;
+        // after 3 seconds, redirect to the course page
+        setTimeout(() => {
+            router.push(`/api/course/${courseId}/file/${fileId}`);
+        }, 3*1000);
+        setDownloaded(true);
+    });
+  
     return (
-        <>
-            <h1>Download File Page</h1>
-            <p>Foo: {props.foo}</p>
-            <p>Course ID: {props.courseId}</p>
-            <p>File ID: {props.fileId}</p>
-        </>
-    )
-}
+      <div className='p-4 flex flex-col gap-2 text-center'>
+        <h1 className='text-main-title-size font-semibold text-primary-600 text-center my-3 px-5 w-full text-center text-ellipsis break-words mb-6'>
+            Downloading the file: {fileName}, please wait...
+        </h1>
+        <p>Foo: {props.foo}</p>
+        <p>Course ID: {props.courseId}</p>
+        <p>File ID: {fileId}</p>
+        <div className="flex flex-row gap-4 mx-auto text-white">
+            <Link href={`/api/course/${courseId}/file/${fileId}`} className='px-4 py-2 bg-primary-600 hover:bg-primary-500 rounded-md'>
+                Download
+            </Link>
+
+            <Link href={`/course/${courseId}`} className='px-4 py-2 bg-gray-500 hover:bg-gray-400 rounded-md'>
+                Back to course
+            </Link>
+        </div>
+        
+      </div>
+    );
+  }
