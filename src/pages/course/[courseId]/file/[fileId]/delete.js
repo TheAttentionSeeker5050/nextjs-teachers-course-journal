2bd46e607ffd246ad397b646e854e200c8aa1a52
Oladoyin <doyinma@gmail.com>
2024-03-26 04:58:13 -0300

    created new note files

--- src/pages/course/[courseId]/file/[fileId]/delete.js
+++ src/pages/course/[courseId]/file/[fileId]/delete.js
@@ -17,7 +17,7 @@ export async function getServerSideProps(context) {
         // search the file on db record
         const fileRecord = await prisma.fileUpload.findUnique({
             where: {
-            id: parseInt(fileId),
+                id: parseInt(fileId),
             },
         });
 
@@ -56,7 +56,7 @@ export async function getServerSideProps(context) {
             }
         }
     }
-    
+
 }
 
 export default function DeleteFileAttachment(props) {
@@ -64,7 +64,7 @@ export default function DeleteFileAttachment(props) {
     const router = useRouter();
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState(null);  
+    const [message, setMessage] = useState(null);
 
     // function to handle submit
     const handleSubmit = async (e) => {
@@ -92,44 +92,44 @@ export default function DeleteFileAttachment(props) {
             // after 3 seconds, redirect to the course page
             setTimeout(() => {
                 router.push(`/course/${courseId}`);
-            }, 3*1000);
+            }, 3 * 1000);
 
         } catch (error) {
             setLoading(false);
             setError(error.message);
         }
     }
-    
-    return (
-        <> 
-        {
-            loading === true ? <div className='text-center'>Loading...</div>
-            :
-            <div className='p-4 flex flex-col gap-2 text-center'>
-                
-                <h1 className='text-main-title-size font-semibold text-primary-600 text-center my-3 px-5 w-full text-center text-ellipsis break-words mb-6'>
-                    Are you sure you want to delete the file?
-                </h1>
-                { error ? 
-                    <div className='text-red-500'>{error}</div> 
-                : message ? 
-                    <div className='text-green-500'>{message}</div> 
-                : null }
 
-                <p>File name: {props.fileName}</p>
-                <form className="flex flex-row gap-4 mx-auto text-white" onSubmit={handleSubmit}>
-                    <button type='submit' className='px-4 py-2 bg-primary-600 hover:bg-primary-500 rounded-md'>
-                        Download
-                    </button>
-        
-                    <Link href={`/course/${courseId}`} className='px-4 py-2 bg-gray-500 hover:bg-gray-400 rounded-md'>
-                        Back to course
-                    </Link>
-                </form>
-            
-            </div>
-        }
+    return (
+        <>
+            {
+                loading === true ? <div className='text-center'>Loading...</div>
+                    :
+                    <div className='p-4 flex flex-col gap-2 text-center'>
+
+                        <h1 className='text-main-title-size font-semibold text-primary-600 text-center my-3 px-5 w-full text-center text-ellipsis break-words mb-6'>
+                            Are you sure you want to delete the file?
+                        </h1>
+                        {error ?
+                            <div className='text-red-500'>{error}</div>
+                            : message ?
+                                <div className='text-green-500'>{message}</div>
+                                : null}
+
+                        <p>File name: {props.fileName}</p>
+                        <form className="flex flex-row gap-4 mx-auto text-white" onSubmit={handleSubmit}>
+                            <button type='submit' className='px-4 py-2 bg-red-600 hover:bg-red-500 rounded-md'>
+                                Delete
+                            </button>
+
+                            <Link href={`/course/${courseId}`} className='px-4 py-2 bg-gray-500 hover:bg-gray-400 rounded-md'>
+                                Back to course
+                            </Link>
+                        </form>
+
+                    </div>
+            }
         </>
-      );
+    );
 }
 
