4e44c6f862b5fe88593d4e5c6eba2209498e81e0
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-03-31 14:43:57 -0300

    edited the add new course form to allow thumbmail and hide course

--- src/pages/course/add.js
+++ src/pages/course/add.js
@@ -2,34 +2,74 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Navbar from '@/components/Navbar';
 import { Inter } from "next/font/google";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function AddCourse() {
     const [courseName, setCourseName] = useState('');
+    const [thumbnail, setThumbnail] = useState('');
+    const [hideCourse, setHideCourse] = useState(false);
+    
+    // form response
     const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // set is loading
+        setIsLoading(true);
+
+        // set form data
+        const formData = new FormData();
+        formData.append('courseName', courseName);
+        formData.append('thumbnail', thumbnail);
+        formData.append('hideCourse', hideCourse);
+
         try {
+
+            // validate course name
+            if (courseName.trim() === '') {
+                setErrorMessage('Course name is required');
+                return;
+            }
+
+            // validate thumbnail
+            if (thumbnail === '') {
+                setErrorMessage('Thumbnail is required');
+                return;
+            }
+
             const response = await fetch('/api/course/new', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ courseName }),
+                // headers: {
+                //     'Content-Type': 'application/json'
+                // },
+                body: formData
             });
+
+            // print the json response
+            const jsonResponse = await response.json();
+
             if (response.ok) {
+                setIsLoading(false);
+
                 setSuccessMessage('Course added successfully!');
                 // Redirect after a short delay
                 setTimeout(() => {
                     router.push('/');
                 }, 2000); // Redirect after 2 seconds
+
             } else {
-                console.error('Error adding course:', response.statusText);
+                throw new Error(jsonResponse.error);
             }
+
         } catch (error) {
-            console.error('Error adding course:', error);
+            setErrorMessage('Failed to add course: ' + error.message);
         }
     };
 
@@ -49,8 +89,27 @@ export default function AddCourse() {
                         value={courseName}
                         onChange={(e) => setCourseName(e.target.value)}
                         className="border border-gray-400 rounded px-3 py-2 mb-4"
-                        required
                     />
+
+                    <label htmlFor="thumbnail" className="text-lg mb-2">Course Thumbnail:</label>
+                    <input
+                        id="thumbnail"
+                        type="file"
+                        className="border border-gray-400 rounded px-3 py-2 mb-4"
+                        onChange={(e) => setThumbnail(e.target.files[0])}
+                    />
+
+                    <div className="flex items-center  gap-3 mb-4">
+                        <label htmlFor="hideCourse" className="text-lg">Hide Course:</label>
+                        <input
+                            id="hideCourse"
+                            type="checkbox"
+                            className="border border-gray-400 rounded px-3 py-2"
+                            checked={hideCourse}
+                            onChange={(e) => setHideCourse(e.target.checked)}
+                        />
+                    </div>
+
                     <button type="submit" className="bg-primary-500 text-white px-6 py-2 rounded hover:bg-primary-600">Add Course</button>
                 </form>
             </div>
